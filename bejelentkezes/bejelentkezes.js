7c676c5f991fb78049bf0f0066cb734eb20df7da
tothbalazsipari <animefan199914@gmail.com>
2024-11-25 11:46:26 +0100

    alap

--- bejelentkezes/bejelentkezes.js
+++ bejelentkezes/bejelentkezes.js
@@ -1,11 +1,27 @@
-function regisztracio(){
+async function regisztracio(){
     let jelszo=document.getElementById("password").value;
     let jelszoBiztos =document.getElementById("passwordMegint").value;
     let felhasznalonev = document.getElementById("felhasznalonev").value;
     let email = document.getElementById("email").value;
 
     if(jelszo.trim().length != 0 ||jelszoBiztos.trim().length != 0  ||felhasznalonev.trim().length != 0 ||email.trim().length != 0 ){
-
+        try {
+            let kuldes = await fetch("./", {
+                method: "POST",
+                headers : {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "felhnev": felhasznalonev,
+                    "email": email,
+                    "jelszo" : jelszo
+                })
+            })
+            let valasz = kuldes.json();
+            console.log(valasz)
+        } catch (error) {
+            console.log(error);
+        }
     }
     else{
         alert("Kérem minden adatott töltsön ki!");
