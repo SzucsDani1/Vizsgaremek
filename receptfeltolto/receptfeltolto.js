864398ef4317ff6e51e5f7f116fe71b3ee6141a6
SzucsDani1 <szucsdaniel1@gmail.com>
2025-02-20 21:37:15 +0100

    konflict kezelés

--- receptfeltolto/receptfeltolto.js
+++ receptfeltolto/receptfeltolto.js
@@ -1076,14 +1076,9 @@ function kepfeltolto() {
             const kepNev = falj.name; 
             feltoltottKepek.push(kepNev);
 
-<<<<<<< HEAD
-            const colDiv = document.createElement('div');
-            colDiv.className = 'col-12 col-lg-3 col-md-6 col-sm-6 position-relative mb-3';
-=======
           
           const colDiv = document.createElement('div');
           colDiv.className = 'col-12 col-lg-3 col-md-6 col-sm-6 position-relative my-3';
->>>>>>> 4c56aeaab2cb32c2ad0a3b4fc8420fbfda35f001
   
             const img = document.createElement('img');
             img.src = e.target.result;
@@ -1091,15 +1086,6 @@ function kepfeltolto() {
             img.style.height = '300px';
             img.style.width = "auto";
 
-<<<<<<< HEAD
-            const btnTorles = document.createElement('button');
-            btnTorles.className = 'btn btn-danger btn-sm position-absolute top-0 start-0';
-            btnTorles.innerHTML = '&times;';
-            btnTorles.addEventListener('click', function() {
-                colDiv.remove();
-                feltoltottKepek = feltoltottKepek.filter(nev => nev !== kepNev);
-            });
-=======
           const btnTorles = document.createElement('button');
           btnTorles.className = 'btn btn-danger btn-sm position-absolute top-0 start-0 ';
           btnTorles.innerHTML = '&times;';
@@ -1107,7 +1093,6 @@ function kepfeltolto() {
             colDiv.remove();
             feltoltottKepek = feltoltottKepek.filter(nev => nev !== kepNev);
           });
->>>>>>> 4c56aeaab2cb32c2ad0a3b4fc8420fbfda35f001
   
             colDiv.appendChild(img);
             colDiv.appendChild(btnTorles);
