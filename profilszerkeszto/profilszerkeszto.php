40d974b32cb9d727ae55b70e8aa0583bf60deb07
tothbalazsipari <animefan199914@gmail.com>
2025-03-17 11:54:09 +0100

    config létrehozva, ezt kell meghívni a session hőz

--- profilszerkeszto/profilszerkeszto.php
+++ profilszerkeszto/profilszerkeszto.php
@@ -1,11 +1,5 @@
 <?php
-    session_set_cookie_params([
-        'lifetime' => 2592000,  // egy hónap mp-ben (30 * 86400)
-        'path' => '/',          // Elérhető legyen az aloldalokon
-        'httponly' => false      // engedélyezi a JavaScript hozzáférést
-    ]);
-    session_start(); 
-   
+     require_once "./adatbazisInterakciok/sessionConfig.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
