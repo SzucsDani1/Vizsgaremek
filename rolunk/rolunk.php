53637761218b3a37c4e7974873af6466f08441cb
tothbalazsipari~ <animefan199914@gmail.com>
2025-03-18 22:28:30 +0100

    alap megjelenítés kész és a szöveg is nagyjából majd még Dani hoz kell szöveg + képek keresése

---
+++ rolunk/rolunk.php
@@ -0,0 +1,92 @@
+<?php
+    require_once "./adatbazisInterakciok/sessionConfig.php";
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rólunk</title>
+    <link rel="stylesheet" href="rolunk.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    
+    <!--START NAVBAR-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Logo helye</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Főoldal</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Receptek</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Heti menü</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Recept feltöltés</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Rólunk</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Gyerek menü</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Profil</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!--END NAVBAR-->
+
+      
+
+      <div class="container text-center">
+        <div class="row d-flex justify-content-center">
+          <div class="col-md-6 col-sm-12 d-flex justify-content-center my-5">
+            <div class="card" style="width: 18rem;">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Szűcs Dániel</h5>
+                <p class="card-text">Szűcs Dániel vagyok, 19 éves szoftverfejlesztő és tesztelő. Kedvenc programnyelveim: .
+                Terveim szerint egyetemen fogom folytatni tanulmányaimat és az informatikai szektorban szeretnék elhelyezkedni diplomámmal.</p>
+                <a href="https://github.com/SzucsDani1" target="_blank" class="btn btn-primary">Github profil</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-12 d-flex justify-content-center my-5">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Tóth Balázs</h5>
+                  <p class="card-text">Tóth Balázs vagyok, 20 éves szoftverfejlesztő és tesztelő. Kedvenc programnyelveim: php, javascript, c#.
+                    Terveim szerint egyetemen fogom folytatni tanulmányaimat és az informatikai szektorban szeretnék elhelyezkedni diplomámmal.
+                  </p>
+                  <a href="https://github.com/tothbalazsipari" target="_blank" class="btn btn-primary">Github profil</a>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+          
+      
+    
+   
+      
+    
+      
+    <script src="rolunk.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
