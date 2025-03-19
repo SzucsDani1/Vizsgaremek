e20036064bf517fbdd3d0134cb96b0d6789735af
tothbalazsipari~ <animefan199914@gmail.com>
2025-03-19 20:37:45 +0100

    kicsi dizájn + footer

--- profilszerkeszto/profilszerkeszto.php
+++ profilszerkeszto/profilszerkeszto.php
@@ -5,7 +5,7 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="profilszerkeszto.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body id="body">
     
     <!--START NAVBAR-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -168,6 +168,18 @@
       
     </div>
       
+
+    <footer class="footer">
+        <p>Elérhetőségek: 
+          <br>
+          <a href="mailto:">dani</a>
+          <br>
+          <a href="mailto:toth.balazs050313@gmail.com">toth.balazs050313@gmail.com</a>
+      </p>
+        
+        <!-- <p>&copy; 2024 Your Company. All rights reserved.</p> -->
+    </footer>
+    
     <script src="profilszerkeszto.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
