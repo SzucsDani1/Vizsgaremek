5597a897426f6cd070c27f0ab620df70234cee78
SzucsDani1 <szucsdaniel1@gmail.com>
2025-03-24 13:07:42 +0100

    javascript receptbetöltés átalakításának elkezdése

--- hetimenuoldal/hetimenuoldal.js
+++ hetimenuoldal/hetimenuoldal.js
@@ -19,81 +19,83 @@ async function receptek(){
 }
 
 
-function receptekBetoltes(receptek, divContainer){
-    divContainer.innerHTML = "";  
+function receptekBetoltes(receptek){
+    //hetimenuKiir.innerHTML = "";  
   
-    let divRow = document.createElement("div");
-    divRow.classList = "row";
+    /*let divRow = document.createElement("div");
+    divRow.classList = "row";*/
     
-    divContainer.innerHTML = "";
+   /* hetimenuKiir.innerHTML = "";
   
-    divContainer.appendChild(divRow);
+    hetimenuKiir.appendChild(divRow);*/
   
-    let napszakSzamlalo = 0;
+
+    //FÉLKÉSZ A KÓD -> appenchildok még nem jók!!!
     let napSzamlalo = 0;
-    for(let recept of receptek){
-        if(recept.napszak == napszakok[napszakSzamlalo]){
-            let kiir = document.getElementById(napszakok[napszakSzamlalo]+"Megjelenites");
+    let kivalasztottNap = document.querySelector('input[name="hetNapjai"]:checked').value;
+    for(let napszak of napszakok){
+        if(receptek[kivalasztottNap].napszak == napszak){
+            let kiir = document.getElementById(napszak+"Megjelenites");
+
+            let divCard = document.createElement("div");
+            divCard.classList = "card col-12 col-lg-3 p-2 col-md-6 col-sm-12 mx-auto my-3"; 
+            divCard.style = "width: 18rem;";
+            divCard.id = receptek[kivalasztottNap].neve;
+    
+            let img = document.createElement("img");
+            img.src = receptek[kivalasztottNap].kepek;
+            img.classList = "card-img-top";
+            img.alt = receptek[kivalasztottNap].neve;
+            img.width = 250;
+            img.height = 200;
+    
+            let divCardBody = document.createElement("div");
+            divCardBody.classList = "card-body";
+    
+            let h5 = document.createElement("h5");
+            h5.classList = "card-title";
+            h5.innerHTML = receptek[kivalasztottNap].neve;
+    
+            let pJellemzok = document.createElement("p");
+            pJellemzok.classList = "text-body-secondary fw-light";
+            pJellemzok.innerHTML = receptek[kivalasztottNap].kaloria+" kcal | "+ receptek[kivalasztottNap].nehezseg + " | " + receptek[kivalasztottNap].ido + " perc";
+    
+            let br = document.createElement("br");
+    
+            let inputButton = document.createElement("input");
+            inputButton.type = "button";
+            inputButton.classList = "btn btn-warning";
+            inputButton.value = "Részletek";
+    
+            let pFeltolto = document.createElement("p");
+            pFeltolto.classList = "text-body-secondary fw-light mt-2";
+            pFeltolto.innerHTML = receptek[kivalasztottNap].felhnev + "\t|\t"+ receptek[kivalasztottNap].mikor_feltolt;
+    
+            let btnTorles = document.createElement("input");
+            btnTorles.type = "button";
+            btnTorles.id = "btn"+receptek[kivalasztottNap].neve;
+            btnTorles.value = "Törlés";
+            btnTorles.classList = "btn btn-danger w-100";
+            
+            btnTorles.addEventListener("click", function() {
+            console.log("ID: "+receptek[kivalasztottNap].id);
+            kedvencReceptTorles(receptek[kivalasztottNap].id)
+            });
+    
+            divRow.appendChild(divCard);
+    
+            divCard.appendChild(img);
+            divCard.appendChild(divCardBody);
+    
+            divCardBody.appendChild(pJellemzok);
+            divCardBody.appendChild(h5);
+            divCardBody.appendChild(br);
+            divCardBody.appendChild(inputButton);
+            divCardBody.appendChild(pFeltolto);
+            divCardBody.appendChild(btnTorles);
+            
+            napSzamlalo++;
         }
-        let divCard = document.createElement("div");
-        divCard.classList = "card col-12 col-lg-3 p-2 col-md-6 col-sm-12 mx-auto my-3"; 
-        divCard.style = "width: 18rem;";
-        divCard.id = recept.neve;
-  
-        let img = document.createElement("img");
-        img.src = recept.kepek;
-        img.classList = "card-img-top";
-        img.alt = recept.neve;
-        img.width = 250;
-        img.height = 200;
-  
-        let divCardBody = document.createElement("div");
-        divCardBody.classList = "card-body";
-  
-        let h5 = document.createElement("h5");
-        h5.classList = "card-title";
-        h5.innerHTML = recept.neve;
-  
-        let pJellemzok = document.createElement("p");
-        pJellemzok.classList = "text-body-secondary fw-light";
-        pJellemzok.innerHTML = recept.kaloria+" kcal | "+ recept.nehezseg + " | " + recept.ido + " perc";
-  
-        let br = document.createElement("br");
-  
-        let inputButton = document.createElement("input");
-        inputButton.type = "button";
-        inputButton.classList = "btn btn-warning";
-        inputButton.value = "Részletek";
-  
-        let pFeltolto = document.createElement("p");
-        pFeltolto.classList = "text-body-secondary fw-light mt-2";
-        pFeltolto.innerHTML = recept.felhnev + "\t|\t"+ recept.mikor_feltolt;
-  
-        let btnTorles = document.createElement("input");
-        btnTorles.type = "button";
-        btnTorles.id = "btn"+recept.neve;
-        btnTorles.value = "Törlés";
-        btnTorles.classList = "btn btn-danger w-100";
-        
-        btnTorles.addEventListener("click", function() {
-          console.log("ID: "+recept.id);
-          kedvencReceptTorles(recept.id)
-        });
-  
-        divRow.appendChild(divCard);
-  
-        divCard.appendChild(img);
-        divCard.appendChild(divCardBody);
-  
-        divCardBody.appendChild(pJellemzok);
-        divCardBody.appendChild(h5);
-        divCardBody.appendChild(br);
-        divCardBody.appendChild(inputButton);
-        divCardBody.appendChild(pFeltolto);
-        divCardBody.appendChild(btnTorles);
-        
-        napszakSzamlalo++;
-        napSzamlalo++;
     }
   
   }
