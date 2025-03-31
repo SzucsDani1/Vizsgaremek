e48d5be9eadf39816d28284cce6c238ad2a97a36
SzucsDani1 <szucsdaniel1@gmail.com>
2025-03-31 11:49:06 +0200

    navbar css megírása

--- receptekoldal/receptekoldal.php
+++ receptekoldal/receptekoldal.php
@@ -10,7 +10,7 @@
     <title>Receptek</title>
     <link rel="stylesheet" href="receptekoldal.css">
     <link rel="stylesheet" href="../footer.css">
-
+    <link rel="stylesheet" href="../navbar.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body id="body">
--- rolunk/rolunk.php
+++ rolunk/rolunk.php
@@ -9,7 +9,7 @@
     <title>Rólunk</title>
     <link rel="stylesheet" href="./rolunk.css">
     <link rel="stylesheet" href="../footer.css">
-    
+    <link rel="stylesheet" href="../navbar.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body id="body">
