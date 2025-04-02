411b7d43830bddce38b700ad4d2e08bb428c1ce2
SzucsDani1 <szucsdaniel1@gmail.com>
2025-04-02 20:28:21 +0200

    Profilszerkesztő oldalon a módosítás gomb és ha nincs profilképed, annak a feliratnak a kijavítása és a receptek kártyáján lévő tartalom középre igazítása

--- hetimenuoldal/hetimenuoldal.js
+++ hetimenuoldal/hetimenuoldal.js
@@ -118,7 +118,7 @@ async function hetNapjaLeker(){
             img.height = 200;
             
             let divCardBody = document.createElement("div");
-            divCardBody.classList = "card-body";
+            divCardBody.classList = "card-body text-center";
             
             let h5 = document.createElement("h5");
             h5.classList = "card-title";
